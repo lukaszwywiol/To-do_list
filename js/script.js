3930045e29cfd50af01de9e9d0813441b3499504
Lukasz Wywiol <duchgory@poczta.fm>
2023-12-12 14:48:02 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@
     },
   ];
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const removeTask = (index) => {
+    tasks.splice(index, 1);
+    render();
+  };
+
   const render = () => {
     let htmlString = "";
 
@@ -17,21 +30,22 @@
       htmlString += `
           <li
           ${task.done ? ' style="text-decoration: line-through"' : ""}
-          > 
+          >
+          <button class="js-remove">usuń</button>
           ${task.content}
           </li>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
 
-  const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
     });
-
-    render();
   };
 
   const onFormSubmit = (event) => {
